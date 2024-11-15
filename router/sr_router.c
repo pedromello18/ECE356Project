1ef7386907d8557690d367124f291d65653b0b40
Pedro Mello <pgm15@duke.edu>
2024-11-15 04:44:25 -0500

    4:44 make a wish

--- router/sr_router.c
+++ router/sr_router.c
@@ -245,7 +245,8 @@ void sr_handlepacket(struct sr_instance* sr,
     else
     {
       printf("this diva was not cached :(\n");
-      struct sr_arpreq *arpreq = sr_arpcache_queuereq(&sr->cache, p_ip_header->ip_dst, packet, sizeof(packet), interface);
+
+      struct sr_arpreq *arpreq = sr_arpcache_queuereq(&sr->cache, p_ip_header->ip_dst, packet, sizeof(packet), iface_out);
       handle_arpreq(sr, arpreq);
     }
   }
